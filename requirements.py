cf7a0bf73c2af2c183f7728811005d4fd31cdafc
Antariksh Patre <antarikshpatre@gmail.com>
2020-09-27 23:35:10 +0530

    Update requirements.py

--- requirements.py
+++ requirements.py
@@ -1,26 +1,18 @@
 import subprocess
 import sys
+import os
+
 
 def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+    #subprocess.check_call([sys.executable, "-m", "pip", "install", str(package)])
+    os.system("pip install "+ str(package))
+    reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'show', str(package)])
 
+    print(str(reqs) + "\n")
+    print("Installed " + package.upper() + "\n")
+    
+
+install("beautifulsoup4")
 install("selenium")
 install("requests")
-install("beautifulsoup4")
 install("webdriver-manager")
-
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'show', 'selenium'])
-print(reqs)
-print("\n If you can read the above message, SELENIUM has been successfully installed! \n")
-
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'show', 'requests'])
-print(reqs)
-print("\n If you can read the above message, REQUESTS has been successfully installed! \n")
-
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'show', 'beautifulsoup4'])
-print(reqs)
-print("\n If you can read the above message, BEAUTIFUL SOUP 4 has been successfully installed! \n")
-
-reqs = subprocess.check_output([sys.executable, '-m', 'pip', 'show', 'webdriver-manager'])
-print(reqs)
-print("\n If you can read the above message, WEBDRIVER-MANAGER has been successfully installed! \n")
