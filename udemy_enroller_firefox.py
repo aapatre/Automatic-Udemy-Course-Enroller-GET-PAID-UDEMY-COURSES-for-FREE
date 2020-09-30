e4f3ef33294d1ea6d7f1972ac3a62852d15a3f82
Fake ID <junayeed@protonmail.com>
2020-10-01 00:21:22 +0600

    Update udemy_enroller_firefox.py

--- udemy_enroller_firefox.py
+++ udemy_enroller_firefox.py
@@ -1,5 +1,6 @@
 # Install all the requirements by running requirements.py in IDLE or follow the alternate instructions at https://github.com/aapatre/Automatic-Udemy-Course-Enroller-GET-PAID-UDEMY-COURSES-for-FREE/
 # Make sure you have cleared all saved payment details on your Udemy account & the browser!
+# For firefox you need to manually install the driver on Arch Linux (sudo pacman -S geckodriver). Untested on other platforms.
 
 from bs4 import BeautifulSoup
 import requests
