54597dcf6e76fc48ad26446a217d12dc48aac29e
Restyled.io <commits@restyled.io>
2020-10-09 21:13:42 +0000

    Restyled by black

--- udemy_enroller_chromium.py
+++ udemy_enroller_chromium.py
@@ -30,7 +30,8 @@ if "zipcode" in settings["udemy"]:
 languages = settings["udemy"].get("languages")
 
 driver = webdriver.Chrome(
-    ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install())
+    ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()
+)
 
 # Maximizes the browser window since Udemy has a responsive design and the code only works
 driver.maximize_window()
@@ -42,8 +43,7 @@ def getUdemyLink(url):
 
     soup = BeautifulSoup(response.content, "html.parser")
 
-    linkForUdemy = soup.find("span",
-                             class_="rh_button_wrapper").find("a").get("href")
+    linkForUdemy = soup.find("span", class_="rh_button_wrapper").find("a").get("href")
 
     return linkForUdemy
 
@@ -99,8 +99,9 @@ def redeemUdemyCourse(url):
     # If the user has configured languages check it is a supported option
     if languages:
         locale_xpath = "//div[@data-purpose='lead-course-locale']"
-        WebDriverWait(driver, 10).until(EC.presence_of_element_located(
-            (By.XPATH, locale_xpath)))
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.XPATH, locale_xpath))
+        )
 
         locale_element = driver.find_element_by_xpath(locale_xpath)
         if locale_element.text not in languages:
@@ -109,26 +110,29 @@ def redeemUdemyCourse(url):
 
     # Enroll Now 1
     element_present = EC.presence_of_element_located(
-        (By.XPATH, "//button[@data-purpose='buy-this-course-button']"))
+        (By.XPATH, "//button[@data-purpose='buy-this-course-button']")
+    )
     WebDriverWait(driver, 10).until(element_present)
 
     udemyEnroll = driver.find_element_by_xpath(
-        "//button[@data-purpose='buy-this-course-button']")  # Udemy
+        "//button[@data-purpose='buy-this-course-button']"
+    )  # Udemy
     udemyEnroll.click()
 
     # Enroll Now 2
-    element_present = EC.presence_of_element_located((
-        By.XPATH,
-        '//*[@class="udemy pageloaded"]/div[1]/div[2]/div/div/div/div[2]/form/div[2]/div/div[4]/button',
-    ))
+    element_present = EC.presence_of_element_located(
+        (
+            By.XPATH,
+            '//*[@class="udemy pageloaded"]/div[1]/div[2]/div/div/div/div[2]/form/div[2]/div/div[4]/button',
+        )
+    )
     WebDriverWait(driver, 10).until(element_present)
 
     # Check if zipcode exists before doing this
     if zipcode:
         # Assume sometimes zip is not required because script was originally pushed without this
         try:
-            zipcode_element = driver.find_element_by_id(
-                "billingAddressSecondaryInput")
+            zipcode_element = driver.find_element_by_id("billingAddressSecondaryInput")
             zipcode_element.send_keys(zipcode)
 
             # After you put the zip code in, the page refreshes itself and disables the enroll button for a split second.
@@ -150,11 +154,9 @@ def main_function():
     while True:
 
         print("Please Wait: Getting the course list from tutorialbar.com...")
-        print("Page: " + str(page) + ", Loop run count: " +
-              str(loop_run_count))
+        print("Page: " + str(page) + ", Loop run count: " + str(loop_run_count))
 
-        url = "https://www.tutorialbar.com/all-courses/" + "page/" + str(
-            page) + "/"
+        url = "https://www.tutorialbar.com/all-courses/" + "page/" + str(page) + "/"
         courses = getTutorialBarLinks(url)
 
         udemyLinks = gatherUdemyCourseLinks(courses)
@@ -172,13 +174,13 @@ def main_function():
             except BaseException as e:
                 print(
                     "Unable to enroll for this course either because you have already claimed it or the browser "
-                    "window has been closed!")
+                    "window has been closed!"
+                )
 
         page = page + 1
         loop_run_count = loop_run_count + 1
 
-        print(
-            "Moving on to the next page of the course list on tutorialbar.com")
+        print("Moving on to the next page of the course list on tutorialbar.com")
 
 
 try:
