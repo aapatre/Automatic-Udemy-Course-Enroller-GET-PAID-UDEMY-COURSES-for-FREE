d4512e6f7336b0c505e5ee320146377467dcca9d
Restyled.io <commits@restyled.io>
2020-10-11 14:13:59 +0000

    Restyled by reorder-python-imports

--- udemy_enroller_firefox.py
+++ udemy_enroller_firefox.py
@@ -8,7 +8,8 @@ from multiprocessing.dummy import Pool
 import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
