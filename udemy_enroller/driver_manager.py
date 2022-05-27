e85b13e46331c6b73fb46decedde5ca6e5893025
Abishek Ayyappan <103653436+nottrif@users.noreply.github.com>
2022-05-27 12:16:01 +0530

    Update driver_manager.py

--- udemy_enroller/driver_manager.py
+++ udemy_enroller/driver_manager.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options as ChromeOptions
-from webdriver_manager.core.utils import ChromeType
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 from webdriver_manager.firefox import GeckoDriverManager
 from webdriver_manager.microsoft import EdgeChromiumDriverManager, IEDriverManager
 from webdriver_manager.opera import OperaDriverManager
