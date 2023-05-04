8ac89874142d4d1491a46b274cfd81c75c638783
Md. Samin Irtiza <62540152+samin-irtiza@users.noreply.github.com>
2023-05-05 01:32:33 +0600

    Added no redirecting http get request
    Added a function named http_get_no_redirect that doesn't follow redirect and returns raw http response.

--- udemy_enroller/http_utils.py
+++ udemy_enroller/http_utils.py
@@ -23,3 +23,21 @@ async def http_get(url, headers=None):
                 return text
     except Exception as e:
         logger.error(f"Error in get request: {e}")
+
+
+async def http_get_no_redirect(url, headers=None):
+    """
+    Send REST get request to the url passed in but doesn't follow redirect.
+
+    :param url: The Url to get call get request on
+    :param headers: The headers to pass with the get request
+    :return: data if any exists
+    """
+    if headers is None:
+        headers = {}
+    try:
+        async with aiohttp.ClientSession() as session:
+            async with session.get(url, headers=headers, allow_redirects=False) as response:
+                return response
+    except Exception as e:
+        logger.error(f"Error in get request: {e}")
